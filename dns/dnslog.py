970a7fd129c44408a3ceb524ae2ace2f40771bc9
omicronns <omicronns@gmail.com>
2021-12-14 23:51:47 +0100

    dns reply

--- dns/dnslog.py
+++ dns/dnslog.py
@@ -2,7 +2,7 @@ import socket
 import sys
 import re
 import binascii
-from dnslib import DNSRecord
+from dnslib import DNSRecord, RR, A
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 53
@@ -14,12 +14,14 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    byteData, addr = sock.recvfrom(2048)
+    data, addr = sock.recvfrom(2048)
     try:
-        msg = binascii.unhexlify(binascii.b2a_hex(byteData))
-        pkt = DNSRecord.parse(msg)
+        pkt = DNSRecord.parse(data)
         with open(sys.argv[1], "a") as log:
             print('question: {}'.format(pkt.get_q().get_qname()), file=log)
+        rsp = pkt.reply()
+        rsp.add_answer(RR(pkt.get_q().get_qname(), rdata=A("127.0.0.1")))
+        sock.sendto(rsp.pack(), addr)
     except Exception as e:
         print(e)
         continue

